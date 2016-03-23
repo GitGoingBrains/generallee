4c7fae226b4b8b1f423caad576af96fb6788b7b7
gitgoingbrains <osterlin@gmail.com>
2016-03-23 03:46:11 -0400

    fixes

--- nodejs.sh
+++ nodejs.sh
@@ -1,9 +1,9 @@
 #1/bin/bash
-L https://deb.nodesource.com/setup_5.x | sudo -E bash -
-sudo apt-get install -y nodejs
+curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash -
+#sudo apt-get install -y nodejs
 
 #sudo apt-get install -y nodejs
-echo "nodejs done" nodejs --version >> setup-args.log
+echo "nodejs done" node --version >> setup-args.log
 #sudo apt-get install -y npm
 echo "npm done" npm --version >> setup-args.log
 exit
